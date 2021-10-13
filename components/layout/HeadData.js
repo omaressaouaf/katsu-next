7b6a735478c4f301dbbb318196ac8ce5e9cad52f
Omar-Ess <souafomar@gmail.com>
2021-10-13 14:13:19 +0100

    updated title and description in meta data

--- components/layout/HeadData.js
+++ components/layout/HeadData.js
@@ -40,17 +40,17 @@ const HeadData = () => {
         rel="stylesheet"
       />
 
-      <title>Katsu Sushi - Meilleur restaurant asiatique à casablanca</title>
+      <title>Katsu Maroc - Sushi Livraison à domicile sur casablanca</title>
       <meta
-        content="Livraison de sushi des Katsu, sushi rolls, sushi box, et plus à casablanca. Commandez maintenant auprès du meilleur restaurant asiatique"
+        content="Livraison à domicile de sushi sur casablanca. Commander chez katsu maroc le meilleur restaurant asiatique"
         name="description"
       />
       <link rel="canonical" href="https://www.katsu.ma/" />
       <meta property="og:type" content="article" />
-      <meta property="og:title" content="Katsu Sushi - Meilleur restaurant asiatique à casablanca" />
+      <meta property="og:title" content="Katsu Maroc - Sushi Livraison à domicile sur casablanca" />
       <meta
         property="og:description"
-        content="Livraison de sushi des Katsu, sushi rolls, sushi box, et plus à casablanca. Commandez maintenant auprès du meilleur restaurant asiatique"
+        content="Livraison à domicile de sushi sur casablanca. Commander chez katsu maroc le meilleur restaurant asiatique"
       />
       <meta property="og:image" content="/assets/img/logo.png" />
       <meta property="og:url" content="https://www.katsu.ma/" />
@@ -59,8 +59,8 @@ const HeadData = () => {
       <meta name="twitter:image:alt" content="Katsu Logo" />
       <meta
         name="keywords"
-        content="katsu, Katsu, katsu sushi, sushi, katsu maroc, katsu casablanca, katsu restaurant, sushi restaurant, sushi restaurants, restaurant asiatique, livraison à casablanca, Restaurant asiatique SushiThai, Sushis à casablanca, sushi casablanca prix, sushi club casablanca, sushi gourmet casablanca, yoka sushi casablanca, sushi casablanca maarif, sushi casablanca menu, cuisine asiatique, sushi menu, best sushi,sushi casablanca,fast food casablanca livraison, sushi gourmet, soy restaurant casablanca menu, sushi kiosque casablanca, sushi livraison casablanca,sushi casablanca, livraison gratuite casablanca, healthy food, nourriture, sushi near me, fast food, fast food casablanca, fast food restaurant, fast food restaurant near me, jumia food, sushi club, sushi box rabat, sushi bar, 	sushi a volonté casablanca, sushi à la maison, sushi casablanca menu, sushi crunchy, sushi express, food morocco, moroccan food, jumia food maroc, menu, sushi box, sushi rolls, starters, soupes, tacos, tataki, dessert, salades, tartares, chirashi, sashimi, nigiri, gunkan, classic rolls, fresh rolls, maki, futomaki, spring rolls, katsu rolls, crunchy, yakitori, bentos, plats, desserts, boissons, bon appétit, meilleur sushi, meilleurs sushis, sourceup, source up, SourceUp, Source Up, service traiteur, dounia batma, leila hadioui, sonia ngadi, bouchra ddeau, nouhaila barbie, Katsu, taste the difference, Rue Sebou, rue sebou, 17 Rue Sebou,
-      Casablanca 20250"
+        content="katsu, Katsu, katsu sushi, sushi, katsu maroc, katsu casablanca, katsu restaurant, sushi restaurant, sushi restaurants,sushi casablanca livraison, restaurant asiatique, livraison à casablanca, Restaurant asiatique SushiThai, Sushis à casablanca, sushi casablanca prix, sushi club casablanca, sushi gourmet casablanca, yoka sushi, yoka sushi casablanca, sushi casablanca maarif, sushi casablanca menu, cuisine asiatique, sushi menu, best sushi,sushi casablanca, sushi gourmet, soy restaurant casablanca menu, sushi kiosque casablanca, sushi livraison casablanca, sushi livraison, sushi livraison à domicile, sushi casablanca, livraison gratuite casablanca, sushi near me, jumia food, sushi club, sushi box rabat, sushi bar, 	sushi a volonté casablanca, sushi à la maison, sushi casablanca menu, sushi crunchy, sushi express, sushi box, sushi rolls, tataki, tartares, chirashi, sashimi, nigiri, gunkan, classic rolls, fresh rolls, sushi rolls, maki, futomaki, spring rolls, katsu rolls, crunchy, yakitori, bentos, meilleur sushi, meilleurs sushis, service traiteur sushi, dounia batma, leila hadioui, sonia ngadi, bouchra ddeau, nouhaila barbie, Rue Sebou, rue sebou, 17 Rue Sebou,
+        Casablanca 20250"
       />
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }} />
     </Head>
